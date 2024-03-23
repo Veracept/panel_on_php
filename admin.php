8f3bd139a9f17405c0416564f48597b660a2593e
Veracept <106467557+Veracept@users.noreply.github.com>
2024-03-23 16:41:18 +0100

    Update admin.php

--- admin.php
+++ admin.php
@@ -4,13 +4,14 @@ require_once __DIR__ . '/src/helpers.php';
 checkAuth();
 checkAuthorizedUserId($allowedIds);
 
+$user = currentUser();
 ?>
 
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet">
 
     <link rel="stylesheet" href="css/owl.carousel.min.css">
@@ -22,11 +23,33 @@ checkAuthorizedUserId($allowedIds);
     
     <!-- Style -->
     <link rel="stylesheet" href="css/style.css">
+    <title>Админ Раздел</title>
+    <style>
 
-    <title>Таблица Модерации[Admin]</title>
-  </head>
-  <body>
-    <header>
+@media screen and (max-width: 768px) {
+  .inner-header h1 {
+    display: none;
+  }
+}
+
+/* Расположить текст выше кнопок на мобильных устройствах */
+@media screen and (max-width: 768px) {
+  .inner-header {
+    flex-direction: column;
+    align-items: flex-start;
+  }
+}
+
+@media screen and (max-width: 768px) {
+  .container {
+    margin-right: 0 !important;
+  }
+}
+
+    </style>
+</head>
+<body>
+<header>
       <div class="collapse bg-dark" id="navbarHeader">
         <div class="container">
           <div class="row">
@@ -57,68 +80,47 @@ checkAuthorizedUserId($allowedIds);
       </div>
     </header>
 
-  <div class="content">
+
+    <div class="headerr">
+        <div class="inner-header flex">
+        <h1 style="margin-left: 150px"><img src="https://rodina-nexus.com/data/assets/logo/ico.png" style="width: 25; height: 25px;"> Панель Модерации || Восточный Округ</h1>
+
+        <section style="margin-right: -200px;" class="py-5 text-center container">
+            <div class="row py-lg-5">
+              <div class="col-lg-6 col-md-8 mx-auto">
+                <h2 class="fw-light">Добро пожаловать, <?php echo $user['name'] ?> [Admin]</h2>
+                <p class="lead text-muted">Выбери опцию, которая тебе нужна</p>
+                <p>
+                  <a href="admin-table.php" class="btn btn-primary my-2">Таблица Модерации [Admin]</a>
+                  <a href="admin-inactive.php" class="btn btn-secondary my-2">Таблица Неактивов[Admin]</a>
+                  <a href="register.php" class="btn btn-secondary my-2">Регестрация Модератора[Admin]</a>
+                  <a href="addmoder.php" class="btn btn-secondary my-2">Добавить Модератора[Admin]</a>
+                </p>
+              </div>
+            </div>
+          </section>
+
+        </div>
+        <div>
+        <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+        viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+        <defs>
+        <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+        </defs>
+        <g class="parallax">
+        <use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.7)" />
+        <use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.5)" />
+        <use xlink:href="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.3)" />
+        <use xlink:href="#gentle-wave" x="48" y="7" fill="#fff" />
+        </g>
+        </svg>
+        </div>
+        </div>
+        <div class="contentt flex">
+          <p>Keeper Inc 2021 - 2024 </p>
+        </div>
+
+
     
-    <div class="container">
-      <h2 class="forh2">Таблица Модерации || Восточный Округ [Admin]</h2>
-
-      <div class="table-responsive">
-
-<table  class="table table-striped custom-table">
-  <thead>
-    <tr>
-      
-      <th scope="col"> № </th>
-      <th scope="col">Nickname Модератора</th>
-      <th scope="col">Уровень</th>
-      <th scope="col">Действия</th>
-  <tbody>
-
-            <?php
-            // Подключаем файл конфигурации
-            require_once __DIR__ . '/src/config.php';
-
-
-            // Выбираем всех модераторов из базы данных
-            $sql = "SELECT id, nickname, level, notes,
-            CASE 
-                WHEN level = 'Тех.Админ Discord' THEN 1
-                WHEN level = 'Главный Модератор' THEN 2
-                WHEN level = 'Зам.Главного Модератора' THEN 3
-                WHEN level = 'Куратор Модерации' THEN 4
-                WHEN level = 'Специальный Модератор' THEN 5
-                WHEN level = 'Модератор 3 lvl' THEN 6
-                WHEN level = 'Модератор 2 lvl' THEN 7
-                WHEN level = 'Модератор 1 lvl' THEN 8
-                ELSE 9
-            END AS level_order
-        FROM moderators
-        ORDER BY level_order ASC";
-            $result = $conn->query($sql);
-            $count = 1;
-
-            if ($result->num_rows > 0) {
-                // Выводим данные в таблицу
-                while($row = $result->fetch_assoc()) {
-                    echo "<tr>";
-                    echo "<td>" . $count++ . '.' . "</td>";
-                    echo "<td>" . $row['nickname'] . "</td>";
-                    echo "<td>" . $row['level'] . "<small class='d-block'>" . $row['notes'] . "</small></td>";
-                    echo "<td>";
-                    // Ссылка на страницу редактирования модератора
-                    echo "<a href='updatemoder.php?id=" . $row['id'] . "'>Редактировать</a>";
-                    // Ссылка на страницу удаления модератора
-                    echo " | <a href='src\actions\deletemoder.php?id=" . $row['id'] . "'>Удалить</a>";
-                    echo "</td>";
-                    echo "</tr>";
-                }
-            } else {
-                echo "<tr><td colspan='2'>Нет данных</td></tr>";
-            }
-            $conn->close();
-            ?>
-        </tbody>
-    </table>
-<br>
 </body>
 </html>
